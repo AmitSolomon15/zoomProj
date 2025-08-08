881b26c81c8fdc2ff00f7a03952f12729830bdde
AmitSolomon15 <amitsol462@gmail.com>
2025-08-08 22:12:03 +0300

    console dialog

--- react/zoomProjFront/src/components/SendDataToSock.jsx
+++ react/zoomProjFront/src/components/SendDataToSock.jsx
@@ -1,4 +1,7 @@
 const socket = new WebSocket("https://zoomproj-back-ws.onrender.com/ws");
+const form = new FormData()
+const username = document.querySelector(".name").innerText;
+form.append("username",username)
 
 function SendDataToSock(){
   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
@@ -8,7 +11,7 @@ function SendDataToSock(){
       recorder.ondataavailable = (event) => {
         if (event.data.size > 0 && socket.readyState === WebSocket.OPEN) {
           socket.send(event.data);
-          
+          socket.send(form)
         }
       };
 
