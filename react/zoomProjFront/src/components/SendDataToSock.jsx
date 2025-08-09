651e5f2cc6a492c6fe289365305e10a8e4f01ce1
AmitSolomon15 <amitsol462@gmail.com>
2025-08-10 02:31:41 +0300

    sockets fix debugging

--- react/zoomProjFront/src/components/SendDataToSock.jsx
+++ react/zoomProjFront/src/components/SendDataToSock.jsx
@@ -4,14 +4,13 @@ import axios from "axios";
 const socket = new WebSocket("wss://zoomproj-back-ws.onrender.com/ws");
 function SendDataToSock(){
   console.log("IM HERE1");
-  function init(){
+  
     
-    const username = localStorage.getItem("nUsername");
-    socket.addEventListener("open",() =>{
-      socket.send(JSON.stringify({username}));
-    });
-  }
-  init();
+  const username = localStorage.getItem("nUsername");
+  socket.addEventListener("open",() =>{
+    socket.send(JSON.stringify({username}));
+  });
+  
   console.log("IM HERE2");
   navigator.mediaDevices.getUserMedia({ video: true, audio: true })
     .then(stream => {
