5372f45e8c9446c6dbd8a0574d70691d7e843100
AmitSolomon15 <amitsol462@gmail.com>
2025-08-24 09:40:50 +0300

    video debugging

--- react/zoomProjFront/src/components/SendDataToSock.jsx
+++ react/zoomProjFront/src/components/SendDataToSock.jsx
@@ -12,8 +12,9 @@ function SendDataToSock(){
   let source;
 
   mediaSource.addEventListener("sourceopen", () => {
+    console.log("MEDIA SOURCE OPENED");
     source = mediaSource.addSourceBuffer('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-    video.play();
+    //video.play();
   });
 
   socket.addEventListener("message", (event)=>{
