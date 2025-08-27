e3c6598b218dbf6ce3ecfbe891d4ef4a0ca02652
AmitSolomon15 <amitsol462@gmail.com>
2025-08-27 17:39:36 +0300

    video debugging

--- go/webSocketsServer/webSocketsServer.go
+++ go/webSocketsServer/webSocketsServer.go
@@ -259,13 +259,14 @@ func findReciever(sender string) (string, error) {
 
 func forwordToReciver(sender string) {
 	fmt.Println("forwordToReciver")
-	receiver, err := findReciever(sender)
-	if err != nil {
-		fmt.Println("ERROR ", err)
-		return
-	}
-	receiverConn := clients[receiver].Conn
+
 	go func() {
+		receiver, err := findReciever(sender)
+		if err != nil {
+			fmt.Println("ERROR ", err)
+			return
+		}
+		receiverConn := clients[receiver].Conn
 		for chunk := range ffmpegOutChan {
 			fmt.Println("CHUNK: ", chunk)
 			mutex.Lock()
