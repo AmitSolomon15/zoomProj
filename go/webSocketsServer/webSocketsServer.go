7cc5cc2afd42d49ea5cfeb48101aa001cbf186ac
AmitSolomon15 <amitsol462@gmail.com>
2025-08-29 20:03:20 +0300

    video debugging

--- go/webSocketsServer/webSocketsServer.go
+++ go/webSocketsServer/webSocketsServer.go
@@ -116,7 +116,7 @@ func cmdInit() {
 	go func() {
 
 		fmt.Println("ENTERED FIRST FUNC")
-		buf := make([]byte, 1024*128)
+		buf := make([]byte, 1024*64)
 		fmt.Println("READ FROM STDOUT")
 		for {
 			//fmt.Println("READABLE BITS ", reader.Buffered())
